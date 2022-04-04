9377c61869f5adb2915a3677e50430aba42272b6
rozim9251 <100237227+rozim9251@users.noreply.github.com>
2022-04-04 12:18:30 +1200

    Add files via upload

--- 01_name_not_blank_v2.py
+++ 01_name_not_blank_v2.py
@@ -15,4 +15,21 @@ def not_blank (question, error_messasge) :
 
 # ***** ain Routine goes here ****
 name = not_blank("Name: ",
+                 "Sorry _ this can't be blank, please enter your name")                 # functoins go here
+
+
+def not_blank (question, error_messasge) :
+    valid = False
+
+    while not valid:
+        response = input (question)
+
+        if response !="":
+             return response
+        else:
+              print(error_messasge)
+
+
+# ***** ain Routine goes here ****
+name = not_blank("Name: ",
                  "Sorry _ this can't be blank, please enter your name")                 
