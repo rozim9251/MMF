5d3f655fa47e0a66d6777a5682d830526ec4c8f2
rozim9251 <100237227+rozim9251@users.noreply.github.com>
2022-07-06 09:50:47 +1200

    Add files via upload

--- RC/04_cost_to_make_v1.py
+++ RC/04_cost_to_make_v1.py
@@ -1,13 +1,10 @@
 # imports...
-
+import math
 
 
 # Functions go here
 
 # checks that string is not blank
-from cgi import print_exception
-
-
 def not_blank (question, error_messasge) :
     valid = False
 
@@ -59,7 +56,6 @@ def choice_checker(question, valid_list, error):
         print(error)
         print()
 
-
 # Asks user all question needed for the receipt calculation
 def cost_for_ingredient():
     
@@ -71,15 +67,14 @@ def cost_for_ingredient():
 
     # asks user for package amount
     package_amount = num_check("Enter Package amount: ", "Sorry, the amount must be a number or a number more than 0")
-   
+    
+    # asks for the unit, if not in list asks again
+    unit = choice_checker ("Enter a unit: ", unit_list, "Invalid response, try again")
+
     # asks user for the cost of ingredient
     price = num_check ("Enter the price: $", "The amount must be a number more than 0")
-   
-    # asks for the unit, if not in list asks again
-    unit = choice_checker ("Enter the unit: ", unit_list, "Invalid response, try again")
-
-    # calculates price over package_amount then times it by quantity
-    cost_to_make = (price / package_amount) * quantity 
+        
+    cost_to_make = ""
 
     return cost_to_make
         
@@ -94,7 +89,9 @@ for item in range(0,4):
     var_item_cost = cost_for_ingredient()
 
     print(var_item_cost)
-    
+
+
+
 
 
 
